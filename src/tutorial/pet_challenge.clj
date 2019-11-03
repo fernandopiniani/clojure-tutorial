a2c7d33791981e8445edd3d5d5b0ea874a33a238
Fernando Piniani <fpiniani@gmail.com>
2019-11-03 20:19:52 -0300

    improve on pet-challenge

--- src/tutorial/pet_challenge.clj
+++ src/tutorial/pet_challenge.clj
@@ -1,8 +1,23 @@
 (ns tutorial.pet-challenge
     (:gen-class))
 
-(def animalMultiplier {:DOG 7, :CAT 5, :FISH 10})
+(defn getAnimalAgeMultiplier
+      "Get multiplier according to animal species"
+      [species]
+      (def animalMultiplier {'DOG 7, 'CAT 5, 'FISH 10})
+      (get animalMultiplier species))
 
-(defn petAgeInHumanYears
-      [age, animal]
-      (* age (get animalMultiplier animal)))
+(defn getPetAgeInHumanYears
+      "Convert pet age in human years"
+      [age, species]
+      (* age (getAnimalAgeMultiplier species)))
+
+(defn printPetAgeInHumanYears
+      "Show conversion result",
+      [age, species]
+      (def humanAge  (getPetAgeInHumanYears age species))
+      (println "The human age for a" species "of" age "is" humanAge "in human years."))
+
+(printPetAgeInHumanYears 10 'DOG)
+(printPetAgeInHumanYears 10 'CAT)
+(printPetAgeInHumanYears 10 'FISH)
